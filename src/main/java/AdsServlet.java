d2928c4ba8096ef649711f70664c448b58d1a396
roxanavp87 <roxanavp87@gmail.com>
2017-06-08 09:51:46 -0500

    Build A Index Page for all of your Ads
    - Create a directory inside webapps named ads
    - Create a file ads/index.jsp. This file will show a listing of all the ads on your site.
    - This page should be available when a user visits /ads.
    - Use the all method on the AdsDao class to get a listing of all the ads, and pass this on to your jsp file
    - In your jsp file, loop through all the ads and display each one.

--- src/main/java/AdsServlet.java
+++ src/main/java/AdsServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by roxana on 6/8/17.
@@ -15,6 +16,8 @@ public class AdsServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        List<Ad> ads = DaoFactory.getAdsDao().all();
+        request.setAttribute("ads", ads);
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
     }
 }
--- src/main/webapp/ads/index.jsp
+++ src/main/webapp/ads/index.jsp
@@ -6,11 +6,17 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Index</title>
 </head>
 <body>
+<ul>
+    <c:forEach var="ad" items="${ads}">
+        <li>${ad.title}</li>
+    </c:forEach>
+</ul>
 
 </body>
 </html>
