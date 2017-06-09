e8dc78b67c9317b9d1069fa197934d27664ab28b
roxanavp87 <roxanavp87@gmail.com>
2017-06-09 11:15:31 -0500

    - Created a class named MySQLAdsDao that implements the Ads interface

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,17 +1,51 @@
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by roxana on 6/9/17.
  */
 public class MySQLAdsDao implements Ads {
+    private Connection connection;
+
+    public MySQLAdsDao() throws SQLException {
+        Config config = new Config();
+        connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUsername(),
+                config.getPassword()
+        );
+    }
 
     @Override
-    public List<Ad> all() {
+    public List<Ad> all() throws SQLException {
+        try {
+            return selectAds(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Long insert(Ad ad) {
         return null;
+    }
+
+    private List<Ad> selectAds(Connection connection) throws SQLException {
+        String query = "SELECT * FROM ads";
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        List<Ad> ads = new ArrayList<>();
+
+        // move the cursor through the results, this while loop will run once for each row in the results
+        while (rs.next()) {
+            int user_id = rs.getInt("user_id");
+            String title = rs.getString("title");
+            String description = rs.getString("description");
+            Ad ad = new Ad(user_id, title, description);
+            ads.add(ad);
+        }
+        return ads;
     }
 }
