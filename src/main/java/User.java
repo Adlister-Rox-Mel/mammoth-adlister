440ba5525f7f5d738ca52e63fe07cd9cad33e313
roxanavp87 <roxanavp87@gmail.com>
2017-06-09 23:27:14 -0500

    - created Users interface

---
+++ src/main/java/User.java
@@ -0,0 +1,38 @@
+/**
+ * Created by roxana on 6/9/17.
+ */
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(int id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -1,38 +1,9 @@
+import java.util.List;
+
 /**
  * Created by roxana on 6/9/17.
  */
-public class Users {
-    private int id;
-    private String username;
-    private String password;
-    private String email;
-
-    public Users(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public Users(int id, String username, String password, String email) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
+public interface Users {
+    public List<User> all();
+    public long insert();
 }
